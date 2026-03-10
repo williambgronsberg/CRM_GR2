fd9e131cdf55bd7b69170fcfee7cf38633fb2a26
William Berge Grønsberg <wil27051@skole.telemarkfylke.no>
2026-03-10 09:20:40 +0100

    Update customer table layout and styles; adjust widths and add font family

---
+++ pages/delete_customer.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @Author: William Berge Groensberg
+ * @Date:   2026-03-03 09:18:02
+ * @Last Modified by:   William Berge Groensberg
+ * @Last Modified time: 2026-03-09 10:57:43
+ */
+?>
