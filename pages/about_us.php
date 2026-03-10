9b4f1cc1926cef36f23917f2ffcda82325115f9d
SRHTNIN <SRHTNIN@proton.me>
2026-03-10 11:10:51 +0100

    La til dummytext på about_us.php.

---
+++ pages/about_us.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @Author: William Berge Groensberg
+ * @Date:   2026-03-03 09:18:02
+ * @Last Modified by:   William Berge Groensberg
+ * @Last Modified time: 2026-03-09 11:20:16
+ */
+require "auth_check.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include "pieces/head.php"; ?>
+</head>
+
+<body>
+	<?php include "pieces/nav.php"; ?>
+
+    <main>
+        <article id="about_us_article">
+            <h2 class="grey_background">Test</h2>
+            <p>Lorem</p>
+            <p>Ipsum</p>
+
+            <h2 class="brown_background">Test</h2>
+            <p>Lorem</p>
+            <p>Ipsum</p>
+
+            <h2 class="green_background">Hva gjør vi?</h2>
+            <p>Det vi gjør hos Frisk AS er å lage pastiller for bedre lukt i din pust!</p>
+            <p>Vi lager egne pustepastiller for våre kunder, slik at hvert firma får sin egen lukt.</p>
+        
+            <h2 class="pink_background">Test</h2>
+            <p>Lorem</p>
+            <p>Ipsum</p>
+        </article>
+    </main>
+</body>
+
+</html>
